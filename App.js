833ea6f40875638c89e429a76374f81e308e7144
unknown <harshitsingh992@gmail.com>
2023-08-22 14:29:48 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,10 @@
+
+
+
+const heading=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"lovleen"},"lovellen"),React.createElement("h2",{id:"lovleen_2"},"lovellen my love")]));
+
+ const root=ReactDOM.createRoot(document. getElementById("root"));
+
+ root.render(heading);
+
+

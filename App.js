fb854ded55643e87db772d118d9031a97288fb31
Harshit singh <100470907+harshitsingh992@users.noreply.github.com>
2024-05-15 01:37:27 +0530

    update

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client";
+
 const heading=React.createElement("div",{id: 'parent'},[React.createElement("div",{id:"child1"},[React.createElement("h2",{},"i am first h2 "),React.createElement("h1",{id:"children"},"hii how are you")]),React.createElement("div",{id:"child2"},[React.createElement("h2",{},"i am first h2 "),React.createElement("h1",{id:"children"},"hii how are you")])]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
